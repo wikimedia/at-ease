738b20d432de1633cfad7a61e2183446b503567a
Kunal Mehta <legoktm@gmail.com>
2015-06-06 13:51:59 -0700

    Initial commit
    Parts of the README were taken from:
     https://www.mediawiki.org/w/index.php?title=Manual:Coding_conventions/PHP&oldid=1662879#Error_handling
    
    Bug: T100923
    Change-Id: Id792d62ed2ae8d04011451459e53d3f88dfea8d7

---
+++ src/Functions.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki;
+
+/**
+ * Reference-counted warning suppression
+ *
+ * @param bool $end
+ */
+function suppressWarnings( $end = false ) {
+	static $suppressCount = 0;
+	static $originalLevel = false;
+
+	if ( $end ) {
+		if ( $suppressCount ) {
+			--$suppressCount;
+			if ( !$suppressCount ) {
+				error_reporting( $originalLevel );
+			}
+		}
+	} else {
+		if ( !$suppressCount ) {
+			$originalLevel = error_reporting( E_ALL & ~(
+					E_WARNING |
+					E_NOTICE |
+					E_USER_WARNING |
+					E_USER_NOTICE |
+					E_DEPRECATED |
+					E_USER_DEPRECATED |
+					E_STRICT
+				) );
+		}
+		++$suppressCount;
+	}
+}
+
+/**
+ * Restore error level to previous value
+ */
+function restoreWarnings() {
+	SuppressWarnings( true );
+}
